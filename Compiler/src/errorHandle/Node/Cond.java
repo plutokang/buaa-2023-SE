090b5467e008c7ba7fef183ae94f7a62ada0b8a6
plutoKang <1420246734@qq.com>
2023-11-08 21:19:31 +0800

    llvm的代码生成1完成，还为写成mip

--- Compiler/src/errorHandle/Node/Cond.java
+++
@@ -1,9 +0,0 @@
-package Node;
-//  Cond → LOrExp
-public class Cond {
-    LOrExp lOrExp;
-    public Cond(LOrExp lOrExp)
-    {
-        this.lOrExp = lOrExp;
-    }
-}
--- Compiler/src/errorHandle/Node/Exp.java
+++
@@ -1,9 +0,0 @@
-package Node;
-//  Exp → AddExp
-public class Exp {
-    public AddExp addExp = null;
-    public Exp(AddExp addExp)
-    {
-        this.addExp = addExp;
-    }
-}
